1ccf3614a9abf40e51b2374687728604eea7edfb
Kirilko-frontend <kkir614@gmail.com>
2025-05-02 12:30:02 +0200

    update build

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120, // Максимальная длина строки перед переносом
+  tabWidth: 2, // Размер таба (по умолчанию 2)
+  useTabs: false, // Использовать пробелы вместо табов
+  semi: true, // Ставить точку с запятой в конце строки
+  singleQuote: true, // Использовать одинарные кавычки
+  htmlWhitespaceSensitivity: "ignore", // Не трогать пробелы в HTML
+  trailingComma: "es5", // Запятые в объектах и массивах (по стандарту ES5)
+  bracketSameLine: false, // В HTML/JSX переносить `>` на новую строку
+};
